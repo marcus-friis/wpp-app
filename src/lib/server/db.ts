31b74050224d6aaca8c49946b2d863ee156095c1
Marcus <marcus.friis99@gmail.com>
2026-09-16 14:31:41 +0200

    web app prototype testing

---
+++ src/lib/server/db.ts
@@ -0,0 +1,14 @@
+import { DuckDBInstance, type DuckDBConnection } from '@duckdb/node-api';
+
+const dbPath = '/Users/marfri/Desktop/advan-test/data/mydb.duckdb';
+
+let connection: DuckDBConnection | undefined;
+
+export async function getConnection() {
+	if (!connection) {
+		const instance = await DuckDBInstance.create(dbPath, { access_mode: 'READ_ONLY' });
+		connection = await instance.connect();
+		await connection.run('INSTALL spatial; LOAD spatial;');
+	}
+	return connection;
+}
